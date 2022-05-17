694b9b696c9b4fd894847abac661e50578fbf0c3
SUNGHO PARK <qwe980813@naver.com>
2022-05-18 02:37:29 +0900

    Feat : 시간표 강의실 및 시간별 조회 테이블 추가 내용 : 시간표 조회 기능 추가 #12

--- src/main/java/deu/team/jsp/schedule/ScheduleRepository.java
+++ src/main/java/deu/team/jsp/schedule/ScheduleRepository.java
@@ -1,11 +1,16 @@
 package deu.team.jsp.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
 
 }
--- src/main/java/deu/team/jsp/schedule/ScheduleService.java
+++ src/main/java/deu/team/jsp/schedule/ScheduleService.java
@@ -1,9 +1,13 @@
 package deu.team.jsp.schedule;
 
 import deu.team.jsp.admin.schedule.dto.ScheduleCreateRequestDto;
+import deu.team.jsp.schedule.dto.StartEndTimeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +22,7 @@ public class ScheduleService {
 
     public void generateSchedule(ScheduleCreateRequestDto requestDto){
         Schedule target = Schedule.builder().day(requestDto.getDay()).startTime(requestDto.getStartTime())
-                .endTime(requestDto.getEndTime()).professor(requestDto.getProfessor()).subject(requestDto.getSubject()).build();
+                .endTime(requestDto.getEndTime()).labNo(requestDto.getLabNo()).professor(requestDto.getProfessor()).subject(requestDto.getSubject()).build();
 
         scheduleRepository.save(target);
     }
@@ -36,5 +40,20 @@ public class ScheduleService {
 
     public Optional<Schedule> getSchedule(Long id){
         return scheduleRepository.findById(id);
+    }
+
+    // 과목이 중복되지 않고 하나만 존재한 경우
+    public List<StartEndTimeDto> getSubjectTime(){
+        if(getScheduleCnt() == 0){
+            return null;
+        }else{
+            List<Schedule> targetScheduleList = scheduleRepository.findAll();
+            List<StartEndTimeDto> targetStartTimeList = new ArrayList<>();
+            for(Schedule target : targetScheduleList) {
+                StartEndTimeDto dto = StartEndTimeDto.builder().labNo(target.getLabNo()).subject(target.getSubject()).startTime(target.getStartTime()).endTime(target.getEndTime()).targetDay(target.getDay()).build();
+                targetStartTimeList.add(dto);
+            }
+            return targetStartTimeList;
+        }
     }
 }
