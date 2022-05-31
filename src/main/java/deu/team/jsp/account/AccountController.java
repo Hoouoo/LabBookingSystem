db8c612fd3f4547f4f3250f54f13b31bd3cd42bb
jsb12302 <jsb12302@naver.com>
2022-05-31 19:18:38 +0900

    Fix : 로그인 시 알림창 오류 수정

--- src/main/java/deu/team/jsp/account/AccountController.java
+++ src/main/java/deu/team/jsp/account/AccountController.java
@@ -3,16 +3,16 @@ package deu.team.jsp.account;
 import deu.team.jsp.OneTimeKey.OneTimeKeyService;
 import deu.team.jsp.account.domain.Role;
 import deu.team.jsp.admin.managelab.ManageLabService;
+import deu.team.jsp.alert.Alert;
 import deu.team.jsp.alert.AlertLastUser;
+import deu.team.jsp.alert.AlertService;
 import deu.team.jsp.interceptor.CheckSession;
 import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,6 +27,9 @@ public class AccountController {
 
     @Autowired
     OneTimeKeyService oneTimeKeyService;
+
+    @Autowired
+    AlertService alertService;
 
     @GetMapping("/")
     public String LoginPage(){
@@ -71,9 +74,16 @@ public class AccountController {
     }
 
     @PostMapping("/signUp")
-    public String SignUp(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        accountService.SignUp(request,response);
-        return "redirect:/";
+        public String SignUp(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String msg="";
+        if(accountService.SignUp(request,response)){
+            msg="회원가입 완료. ";
+        }else{
+            msg="이미 존재하는 학번 이거나 인증키가 일치하지 않습니다. ";
+        }
+        request.setAttribute("msg",msg);
+        request.setAttribute("url","/");
+        return "WEB-INF/alert/alert.jsp";
     }
 
     @CheckSession
